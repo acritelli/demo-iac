35b7e6bd0f3a0a6c81923131b170abb74ce32c99
Anthony Critelli <critellia@gmail.com>
2021-07-24 22:48:41 -0400

    secrets

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,12 +1,19 @@
 # List of IPs that can SSH to servers
-variable ssh_ips {}
+variable ssh_ips {
+}
 
 # API token for Digital Ocean
-variable do_token {}
+variable do_token {
+  sensitve = true
+}
 
 # AWS Credentials
-variable aws_access_key_id {}
-variable aws_secret_access_key {}
+variable aws_access_key_id {
+  sensitive = true
+}
+variable aws_secret_access_key {
+  sensitive = true
+}
 
 # Number of webservers that we want to deploy
 variable webserver_count {
