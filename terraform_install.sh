f682fe858ffd41667545fdb0620d6a575cda0a0a
Anthony Critelli <critellia@gmail.com>
2021-07-24 22:41:14 -0400

    temp dir

--- terraform_install.sh
+++ terraform_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd /tmp
+
 wget https://releases.hashicorp.com/terraform/1.0.3/terraform_1.0.3_linux_amd64.zip
 unzip -o terraform_1.0.3_linux_amd64.zip
 sudo mv -f terraform /bin/terraform
