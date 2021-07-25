51c0fd4d511ad7d97c4000ecc81525a7885b2cb2
Anthony Critelli <critellia@gmail.com>
2021-07-25 08:48:02 -0400

    actions

--- do-inventory.sh
+++ do-inventory.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+current_directory=$(pwd)
+
+echo $current_directory
+
 cd /tmp
 
 rm *do-ansible-inventory*
@@ -8,3 +12,8 @@ wget https://github.com/do-community/do-ansible-inventory/releases/download/v2.0
 
 tar -xf do-ansible-inventory_2.0.0_linux_x86_64.tar.gz
 sudo mv -f do-ansible-inventory /bin/
+
+do-ansible-inventory -t $DO_TOKEN --ignore tf-www.acritelli.com --no-group-by-region --no-group-by-project
+
+cd $current_directory
+pwd
