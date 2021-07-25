ef957101a92af563f286bf02a9a8c97f4ca3644e
Anthony Critelli <critellia@gmail.com>
2021-07-24 22:33:02 -0400

    Clean workspace

--- terraform_install.sh
+++ terraform_install.sh
@@ -2,6 +2,6 @@
 
 wget https://releases.hashicorp.com/terraform/1.0.3/terraform_1.0.3_linux_amd64.zip
 unzip terraform_1.0.3_linux_amd64.zip
-mv terraform /bin/terraform
+sudo mv terraform /bin/terraform
 
 terraform --version
