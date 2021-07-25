0269f99429918603cdcabf6a28ff1b2230a76e3f
Anthony Critelli <critellia@gmail.com>
2021-07-24 22:36:20 -0400

    Add TF Run

--- terraform_install.sh
+++ terraform_install.sh
@@ -2,6 +2,6 @@
 
 wget https://releases.hashicorp.com/terraform/1.0.3/terraform_1.0.3_linux_amd64.zip
 unzip terraform_1.0.3_linux_amd64.zip
-sudo mv terraform /bin/terraform
+sudo mv -f terraform /bin/terraform
 
 terraform --version
